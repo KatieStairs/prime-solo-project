05a33dcdfc4ec69a45afd661a5b254c995b89699
Katie Stairs <kstairs831@gmail.com>
2023-02-08 17:59:31 -0600

    Wrote server get route for home page, added home to NavBar, started HomePage.jsx

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -5,6 +5,7 @@ import {useSelector} from 'react-redux';
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
