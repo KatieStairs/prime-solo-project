423a81adbef30abb63bf6b20168804f7c7754213
Katie Stairs <kstairs831@gmail.com>
2023-02-08 15:32:58 -0600

    Wrote the sql to create the tables I will need.

--- database.sql
+++ database.sql
@@ -8,3 +8,26 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "unfinished_recipes" (
+    "id" SERIAL PRIMARY KEY,
+    "unfinished_recording" VARCHAR (1000) NOT NULL,
+    "recipe_ingredients" VARCHAR (1000) NOT NULL,
+    "recipe_directions" VARCHAR (1000) NOT NULL,
+    "recipe_notes" VARCHAR (1000),
+    "user_id" INTEGER REFERENCES "user"
+);
+
+CREATE TABLE "finalized_recipes" (
+    "id" SERIAL PRIMARY KEY,
+    "finalized_recording" VARCHAR (1000) NOT NULL,
+    "finalized_ingredients" VARCHAR (1000) NOT NULL,
+    "finalized_directions" VARCHAR (1000) NOT NULL,
+    "finalized_notes" VARCHAR (1000),
+    "finalized_photos" VARCHAR (1000),
+    "finalized_user_id" INTEGER REFERENCES "user"
+);
+
+DROP TABLE "user";
+DROP TABLE "unfinished_recipes";
+DROP TABLE "finalized_recipes";
