2d94e43f9dee1f4a1b6492e8bc2f4a98954ee5db
Katie Stairs <kstairs831@gmail.com>
2023-02-09 14:27:41 -0600

    Worked on the Textfields in EditRecipe

--- src/components/EditRecipe/EditRecipe.jsx
+++ src/components/EditRecipe/EditRecipe.jsx
@@ -1,10 +1,37 @@
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 function EditRecipe() {
     
     return (
         <div>
+         <Box
+            mx={6}
+            my={3}
+            border={4}
+            px={2}
+            py={3}
+            borderColor="black"
+            height={300}
+            width={800}
+            display="flex"
+            justifyContent="left"
+            alignItems="left"
+            bgcolor="white"
+            color="black"
+            fontSize={20}
+            >
+            <TextField
+                id="outlined-multiline-static"
+                label="Edit Recipe Ingredients:"
+                multiline
+                minRows={15.5}
+                defaultValue=""
+                fullWidth
+            />
+        <Button variant="contained">Save</Button>
+        </Box>
         <Box
             mx={6}
             my={3}
@@ -20,9 +47,16 @@ function EditRecipe() {
             bgcolor="white"
             color="black"
             fontSize={20}
-        >
-            Edit Recipe Ingredients:
-            {/* <Button variant="contained">Edit</Button> */}
+            >
+        <TextField
+            id="outlined-multiline-static"
+            label="Edit Recipe Directions:"
+            multiline
+            minRows={15.5}
+            defaultValue=""
+            fullWidth
+        />
+        <Button variant="contained">Save</Button>
         </Box>
         <Box
             mx={6}
@@ -40,27 +74,15 @@ function EditRecipe() {
             color="black"
             fontSize={20}
         >
-            Edit Recipe Directions:
-            {/* <Button variant="contained">Edit</Button> */}
-        </Box>
-        <Box
-            mx={6}
-            my={3}
-            border={4}
-            px={2}
-            py={3}
-            borderColor="black"
-            height={300}
-            width={800}
-            display="flex"
-            justifyContent="left"
-            alignItems="left"
-            bgcolor="white"
-            color="black"
-            fontSize={20}
-        >
-            Edit Recipe Notes:
-            {/* <Button variant="contained">Edit</Button> */}
+        <TextField
+            id="outlined-multiline-static"
+            label="Edit Recipe Notes:"
+            multiline
+            minRows={15.5}
+            defaultValue=""
+            fullWidth
+        />
+        <Button variant="contained">Save</Button>
         </Box>
     </div>
     )
