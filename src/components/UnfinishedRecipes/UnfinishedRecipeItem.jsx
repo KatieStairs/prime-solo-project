62c8e74505dab60cedb2a6fc981914ec1ab5529a
Katie Stairs <kstairs831@gmail.com>
2023-02-16 09:02:31 -0600

    Worked on Delete Button.

--- src/components/UnfinishedRecipes/UnfinishedRecipeItem.jsx
+++ src/components/UnfinishedRecipes/UnfinishedRecipeItem.jsx
@@ -1,12 +1,22 @@
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import { Favorite } from '@material-ui/icons';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 function UnfinishedRecipeItem({ unfinished }) {
     const history = useHistory();
+    const dispatch = useDispatch();
 
     const goToEditIngredientsPage = () => {
         history.push(`/EditRecipe/${unfinished.recipe_id}`)
+    }
+
+    const deleteRecipe = () => {
+        dispatch({
+            type: 'SAGA/DELETE_UNFINISHED',
+            payload: unfinished.recipe_id
+        })
     }
 
     return (
@@ -29,7 +39,7 @@ function UnfinishedRecipeItem({ unfinished }) {
             >
             {unfinished.recipe_name} By {unfinished.recipe_author}
             <Button variant="contained" onClick={goToEditIngredientsPage}>Edit Recipe</Button>
-            <Button variant="contained">Share</Button>
+            <Button variant="contained" onClick={deleteRecipe}>Delete</Button>
         </Box>
     )
 }
